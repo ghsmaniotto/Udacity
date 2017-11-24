f4f0eb979c51c8b8e4bb05e5635ebf3d65013567
GustavoSmaniotto <ghsmaniotto@inf.ufpel.edu.br>
2017-11-24 05:16:36 -0200

    First commit

---
+++ Project-5/js/maps.js
@@ -0,0 +1,42 @@
+var map;
+
+var trespassos = {
+    lat: -27.455982,
+    lng: -53.93018
+}
+
+function initMap() {
+    // Constructor creates a new map - only center and zoom are required.
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: trespassos,
+        zoom: 13
+    });
+
+    var marker = addMarker(trespassos, map, "Tres Passos");
+    var infowindow = addInfoWindow(`${marker.getPosition()}`);
+   
+    addListener(map, marker, infowindow);
+}   
+
+
+function addMarker(position, map, title) { 
+    return new google.maps.Marker({
+        position: position,
+        map,
+        title
+    });
+ }
+
+ function addInfoWindow(content) {
+     return new google.maps.InfoWindow({
+         content
+     });
+ }
+
+ function addListener(map, marker, infoWindow) {
+     return marker.addListener(
+         'click',
+        function() {
+            infoWindow.open(map, marker)
+        });
+ }
