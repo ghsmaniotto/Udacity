2a9a3323687221c1bcaa2ce0b600e72ab57aea6b
GustavoSmaniotto <ghsmaniotto@inf.ufpel.edu.br>
2017-12-02 15:47:39 -0200

    Update the methods

--- Project-5/js/app.js
+++ Project-5/js/app.js
@@ -1,84 +1,28 @@
-// MAP DATA
+// Map's data
 var map;
 var infowindow;
 var markersList = [];
-var viewModel = new PlaceListViewModel();
-
 var trespassos = {
     lat: -27.455982,
     lng: -53.93018
 };
-// MAP - CONTENT
 
-function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: trespassos,
-        zoom: 16
-    });
-    infowindow = new google.maps.InfoWindow();
+// Wikipedia's data
+const WIKI_API_URL = "https://en.wikipedia.org/w/api.php";
 
-    // var placeSearchAutoComplete = new google.maps.places.Autocomplete(
-    //     document.getElementById('search-box')
-    // );
-    
-    // placeSearchAutoComplete.bindTo('bounds', map);
+// Foursquare's data
+const FOURSQUARE_API_URL ="https://api.foursquare.com/v2/venues/search";
+const CLIENT_ID = 'ODA5SOZFHKGTQF2GZ2FYPTURKBO1KC1F2QYMTVTCAD2PF5FM'
+const CLIENT_SECRET = 'OIVBCTQFEYIV2KZTI25WJ1MRLWSZEQWAMVOUST2XN4LNIUUJ'
 
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-        location: trespassos,
-        radius: 500,
-        type: ['store']
-    }, callback);
-};
+// Knockout's data
+var viewModel = new PlaceListViewModel();
+ko.applyBindings(viewModel);
 
-function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            markersList.push(createMarker(results[i]));
-        }
-    }
-};
 
-function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-        map: map,
-        position: placeLoc,
-        title: place.name
-    });
-    // var infoWindow = addInfoWindow(place.name)
-    addListener(map, marker, infowindow);
-    viewModel.addPlace(marker);
-    return marker;
-};
-
-function addMarker(position, map, title) {
-    return new google.maps.Marker({
-        position: position,
-        map,
-        title
-    });
-};
-
-function addInfoWindow(content) {
-    return new google.maps.InfoWindow({
-        content
-    });
-};
-
-function addListener(map, marker, infowindow) {
-    return marker.addListener(
-        'click',
-        function () {
-            infowindow.setContent(marker.title);
-            infowindow.open(map, marker);
-        });
-};
-
-/* 
-    Knockout JS
-                */
+/* -----------------------------------------------------------------
+                        Knockout JS
+-------------------------------------------------------------------- */
 
 function Place(marker) {
     this.marker = ko.observable(marker);
@@ -89,6 +33,9 @@ function PlaceListViewModel() {
     var self = this;
     self.places = ko.observableArray();
     self.filter = ko.observable();
+    self.foursquare_checkins_count = ko.observable();
+    self.foursquare_users_count = ko.observable();
+    self.foursquare_tip_count = ko.observable();
 
     // Operations
     self.addPlace = function (marker) {
@@ -98,8 +45,38 @@ function PlaceListViewModel() {
     // Display the info window to specific marker
     self.showInfoWindow = function(place){ 
         // Add content to infowindow
-        infowindow.setContent(place.marker().title);
-        // Display the infowindow
+        var latlng = 
+            place.marker().position.lat().toString() +
+            "," + place.marker().position.lng().toString();
+        // Get the venues response of foursquare and set the infowindow content
+        var foursquare_url = getFoursquareDataFor(latlng, place.marker().title);
+        $.ajax({
+            type: 'GET',
+            url: foursquare_url,
+            dataType: 'jsonp',
+            success: (reqResult) => {
+                console.log(reqResult);
+                if (reqResult.meta &&
+                    reqResult.meta.code === 200 &&
+                    reqResult.response.venues &&
+                    reqResult.response.venues[0]) {
+                    infowindow.setContent(
+                        `<div><h4>Foursquare info's</h4></div>
+                        <div><p>Checkins Count: <span data-bind="text: foursquare_checkins_count">${reqResult.response.venues[0].stats.checkinsCount}</span> </p></div>
+                        <div><p>Users Count: <span data-bind="text: foursquare_users_count">${reqResult.response.venues[0].stats.usersCount}</span> </p></div>
+                        <div><p>Tip Count: <span data-bind="text: foursquare_tip_count">${reqResult.response.venues[0].stats.tipCount}</span> </p></div>`
+                    );
+                    self.foursquare_checkins_count(reqResult.response.venues[0].stats.checkinsCount);
+                    self.foursquare_users_count(reqResult.response.venues[0].stats.usersCount);
+                    self.foursquare_tip_count(reqResult.response.venues[0].stats.tipCount);
+                } else {
+                    infowindow.setContent(`<div><h4>The Foursquare data can't be open. Sorry. Try another marker :D</h4></div>`);
+                }
+            },
+            error: () => {
+                infowindow.setContent(`<div><h4>The Foursquare data can't be open. Sorry. Try another marker :D</h4></div>`);
+            }
+        });
         infowindow.open(map, place.marker());
     };
 
@@ -125,7 +102,102 @@ function PlaceListViewModel() {
         }
     });
 
+};
+// End Knockout section
+
+
+/* -----------------------------------------------------------------
+                        Google maps API
+-------------------------------------------------------------------- */
+function initMap() {
+    // Constructor creates a new map
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: trespassos,
+        zoom: 16
+    });
+    // Create a infowindow to show markers content
+    infowindow = new google.maps.InfoWindow();
+
+    /* 
+        This method will put 20 markers in map according a radius from
+        the map start location and type of points (store, restaurants, ..)
+        This method call an callback method to manage the response of request
+    */
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: trespassos,
+        radius: 500,
+        type: ['store']
+    }, callback);
 
 };
 
-ko.applyBindings(viewModel);
+// This is a google.maps.places.PlacesService callback function
+// This method will create a marker for each selected point in PlacesService()
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            markersList.push(createMarker(results[i]));
+        }
+    }
+};
+
+/* 
+ This method creates a marker into the map.
+ The map, position of the marker (lat, lng) and title are defined.
+ The complete list of parameters to create a marker is available in
+ https://developers.google.com/maps/documentation/javascript/3.exp/reference?hl=pt-br#MarkerOptions
+ */ 
+function createMarker(place) {
+    // Get the marker location
+    var placeLoc = place.geometry.location;
+    // Creates a marker
+    var marker = new google.maps.Marker({
+        map: map,
+        position: placeLoc,
+        title: place.name
+    });
+    // Add a listner to show an infowindow when marker is clicked
+    addListener(map, marker, infowindow);
+    // Add the marker to the view model
+    viewModel.addPlace(marker);
+    return marker;
+};
+
+// Add a listner to show an infowindow when marker is clicked
+function addListener(map, marker, infowindow) {
+    return marker.addListener(
+        'click',
+        function () {
+            // Set the infowindow content
+            infowindow.setContent(marker.title);
+            // Display the infowindow when the button is clicked
+            infowindow.open(map, marker);
+        });
+};
+// End Google API section
+
+/* -----------------------------------------------------------------
+                        Foursquare's API
+-------------------------------------------------------------------- */
+/* 
+    This function creates a url to get the foursquare data for a specific point
+    According to these parementers, the request for the foursquare api
+        returns some data about this place/point
+    Parameters:
+        - The point_latlng parameter is a stirng, formated like
+            -> point_latlng = (lat,lng)
+        - The point_name is a string
+*/
+function getFoursquareDataFor(point_latlng, point_name) {
+    var foursquare_url = FOURSQUARE_API_URL;
+    return foursquare_url += '?' + $.param({
+        "ll": point_latlng,
+        "client_id": "ODA5SOZFHKGTQF2GZ2FYPTURKBO1KC1F2QYMTVTCAD2PF5FM",
+        "client_secret": "OIVBCTQFEYIV2KZTI25WJ1MRLWSZEQWAMVOUST2XN4LNIUUJ",
+        "v": "20171202",
+        "radius": "1",
+        "intent": "match",
+        "name": point_name
+    });
+}
