23644e32b34d2f2c8b9a377fb03010738f885da0
GustavoSmaniotto <ghsmaniotto@inf.ufpel.edu.br>
2017-12-03 16:46:46 -0200

    Add comments in the file

--- Project-5/js/app.js
+++ Project-5/js/app.js
@@ -19,15 +19,23 @@ const CLIENT_SECRET = 'OIVBCTQFEYIV2KZTI25WJ1MRLWSZEQWAMVOUST2XN4LNIUUJ'
 var viewModel = new PlaceListViewModel();
 ko.applyBindings(viewModel);
 
-
 /* -----------------------------------------------------------------
                         Knockout JS
 -------------------------------------------------------------------- */
 
+/**
+* @description Add ko.observable to a marker
+* @param {google.maps.Marker} marker - Marker of a specific place
+*/
 function Place(marker) {
     this.marker = ko.observable(marker);
 };
 
+/**
+* @description Knockout View Model
+    This class describe all functions and variables to knockout
+    will use in the app
+*/
 function PlaceListViewModel() {
     // Data
     var self = this;
@@ -37,25 +45,34 @@ function PlaceListViewModel() {
     self.foursquare_users_count = ko.observable();
     self.foursquare_tip_count = ko.observable();
 
-    // Operations
+    /**
+    * @description Add an place(item) to the places list(observable array)
+    * @param {google.maps.Marker} marker - Map's marker
+    */
     self.addPlace = function (marker) {
         self.places.push(new Place(marker));
     };
 
-    // Display the info window to specific marker
+    /**
+    * @description Display an info window into specific marker
+        The content of the info window is a Foursquare statistic
+        about the specific place
+    * @param {place} title - The title of the book
+    */
     self.showInfoWindow = function(place){ 
-        // Add content to infowindow
+        // Set the lat,lng of the place
         var latlng = 
             place.marker().position.lat().toString() +
             "," + place.marker().position.lng().toString();
         // Get the venues response of foursquare and set the infowindow content
         var foursquare_url = getFoursquareDataFor(latlng, place.marker().title);
+        // Use Ajax to request the foursquare data
         $.ajax({
             type: 'GET',
             url: foursquare_url,
             dataType: 'jsonp',
             success: (reqResult) => {
-                console.log(reqResult);
+                // In success case, show the foursquare content
                 if (reqResult.meta &&
                     reqResult.meta.code === 200 &&
                     reqResult.response.venues &&
@@ -70,16 +87,19 @@ function PlaceListViewModel() {
                     self.foursquare_users_count(reqResult.response.venues[0].stats.usersCount);
                     self.foursquare_tip_count(reqResult.response.venues[0].stats.tipCount);
                 } else {
-                    infowindow.setContent(`<div><h4>The Foursquare data can't be open. Sorry. Try another marker :D</h4></div>`);
+                    infowindow.setContent(`<div"><h6>The Foursquare data can't be found for this place.</h6></div>`);
                 }
             },
             error: () => {
-                infowindow.setContent(`<div><h4>The Foursquare data can't be open. Sorry. Try another marker :D</h4></div>`);
+                infowindow.setContent(`<div><h6>The Foursquare data can't be found for this place.</h6></div>`);
             }
         });
         infowindow.open(map, place.marker());
     };
 
+    /**
+    * @description Filter the places according to gived place name
+    */
     self.filteredItems = ko.computed(function () {
         var filter = self.filter();
         if (!filter) {
@@ -105,10 +125,12 @@ function PlaceListViewModel() {
 };
 // End Knockout section
 
-
 /* -----------------------------------------------------------------
                         Google maps API
 -------------------------------------------------------------------- */
+/**
+    * @description Function to initialize the Google maps
+*/
 function initMap() {
     // Constructor creates a new map
     map = new google.maps.Map(document.getElementById('map'), {
@@ -118,10 +140,11 @@ function initMap() {
     // Create a infowindow to show markers content
     infowindow = new google.maps.InfoWindow();
 
-    /* 
-        This method will put 20 markers in map according a radius from
-        the map start location and type of points (store, restaurants, ..)
-        This method call an callback method to manage the response of request
+    /**
+    * @description This method will put 20 markers in map according a radius from
+            the map start location and type of points (store, restaurants, ..)
+            This method call an callback method to manage the response of request
+    * @param {map} map - Map's marker
     */
     var service = new google.maps.places.PlacesService(map);
     service.nearbySearch({
@@ -132,8 +155,10 @@ function initMap() {
 
 };
 
-// This is a google.maps.places.PlacesService callback function
-// This method will create a marker for each selected point in PlacesService()
+/**
+    * @description  This is a google.maps.places.PlacesService callback function
+        This method will create a marker for each selected point in PlacesService()
+*/
 function callback(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < results.length; i++) {
@@ -142,30 +167,41 @@ function callback(results, status) {
     }
 };
 
-/* 
- This method creates a marker into the map.
+/**
+ * @description This method creates a marker into the map.
  The map, position of the marker (lat, lng) and title are defined.
  The complete list of parameters to create a marker is available in
- https://developers.google.com/maps/documentation/javascript/3.exp/reference?hl=pt-br#MarkerOptions
- */ 
+ https://developers.google.com/maps/documentation/javascript/3.exp/reference?hl=pt-br#MarkerOptionsThis is a google.maps.places.PlacesService callback function
+ */
 function createMarker(place) {
     // Get the marker location
     var placeLoc = place.geometry.location;
+    var newIcon = makeMarkerIcon("FF7400")
     // Creates a marker
     var marker = new google.maps.Marker({
         map: map,
+        icon: newIcon,
         position: placeLoc,
         title: place.name
     });
     // Add a listner to show an infowindow when marker is clicked
-    addListener(map, marker, infowindow);
+    addListenerInfoWindow(map, marker, infowindow);
+    // Add a listner to mouseover
+    addListenerMouseover("5bc0de", marker)
+    // Add a listner to mouseout
+    addListenerMouseout("FF7400", marker)
     // Add the marker to the view model
     viewModel.addPlace(marker);
     return marker;
 };
 
-// Add a listner to show an infowindow when marker is clicked
-function addListener(map, marker, infowindow) {
+/**
+ * @description  Add a listner to show an infowindow when marker is clicked
+ * @param {google.maps.Map} map - Google Maps map
+ * @param {google.maps.marker} marker - Specific marker to add listener
+ * @param {google.maps.InfoWindow} infowindow - The map info window
+ */
+function addListenerInfoWindow(map, marker, infowindow) {
     return marker.addListener(
         'click',
         function () {
@@ -174,20 +210,58 @@ function addListener(map, marker, infowindow) {
             // Display the infowindow when the button is clicked
             infowindow.open(map, marker);
         });
+    };
+    
+/**
+    * @description  Creates a new marker with defined color
+    * @param {String} makerColor - String that contains the color 
+        Must be in the format -> FFFAAA
+*/
+function makeMarkerIcon(markerColor) {
+    var makerImage = new google.maps.MarkerImage(
+        "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + markerColor,
+        new google.maps.Size(31, 54),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(30, 54),
+        new google.maps.Size(31, 54),
+    );
+    return makerImage;
+}; 
+
+/**
+ * @description  Add a listner to change the marker color
+ *   when the mouse is over the marker
+ * @param {String} color - Marker's color formatted as -> AAFFAA
+ * @param {google.maps.marker} marker - Specific marker to add listener
+ */
+function addListenerMouseover(color, marker){
+    return marker.addListener('mouseover', function(){
+        this.setIcon(makeMarkerIcon(color));
+    })
 };
+/**
+ * @description  Add a listner to change the marker color
+ *   when the mouse is out the marker
+ * @param {String} color - Marker's color formatted as -> AAFFAA
+ * @param {google.maps.marker} marker - Specific marker to add listener
+ */
+function addListenerMouseout(color, marker) {
+    return marker.addListener('mouseout', function(){
+        this.setIcon(makeMarkerIcon(color));
+    })
+}
 // End Google API section
 
 /* -----------------------------------------------------------------
                         Foursquare's API
 -------------------------------------------------------------------- */
-/* 
-    This function creates a url to get the foursquare data for a specific point
-    According to these parementers, the request for the foursquare api
+/**
+    * @description  This function creates a url to get the foursquare data for a specific point
+        According to these parementers, the request for the foursquare api
         returns some data about this place/point
-    Parameters:
-        - The point_latlng parameter is a stirng, formated like
-            -> point_latlng = (lat,lng)
-        - The point_name is a string
+    * @param {String} point_latlng - Lat,lng of the place.
+        Must be formated as -> (lat,lng)
+    * @param {String} point_name - Specific marker to add listener
 */
 function getFoursquareDataFor(point_latlng, point_name) {
     var foursquare_url = FOURSQUARE_API_URL;
